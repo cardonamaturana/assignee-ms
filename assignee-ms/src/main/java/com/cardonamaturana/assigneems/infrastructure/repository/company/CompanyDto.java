fa49214f0c8a56df54d53e5dc4325bfb9a731b46
CardonaDeveloperZabud <julio.cardona@zabud.com.co>
2023-09-24 13:45:01 -0500

    state field added into company

--- assignee-ms/src/main/java/com/cardonamaturana/assigneems/infrastructure/repository/company/CompanyDto.java
+++ assignee-ms/src/main/java/com/cardonamaturana/assigneems/infrastructure/repository/company/CompanyDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
 @Document("companies")
@@ -13,6 +14,7 @@ public class CompanyDto {
   @Id
   private String id;
   private String name;
+  private boolean state = true;
   private String nit;
   private String location;
 }
