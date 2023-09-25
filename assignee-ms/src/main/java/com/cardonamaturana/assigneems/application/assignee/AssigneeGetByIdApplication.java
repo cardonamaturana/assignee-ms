dc256f565a55cf5b0d3594c5d4ff9f3131a2b5ea
CardonaDeveloperZabud <julio.cardona@zabud.com.co>
2023-09-24 21:48:04 -0500

    get assignee by id controller

---
+++ assignee-ms/src/main/java/com/cardonamaturana/assigneems/application/assignee/AssigneeGetByIdApplication.java
@@ -0,0 +1,19 @@
+package com.cardonamaturana.assigneems.application.assignee;
+
+import com.cardonamaturana.assigneems.domain.entity.Assignee;
+import com.cardonamaturana.assigneems.domain.service.assignee.AssigneeGetByIdService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class AssigneeGetByIdApplication {
+
+  private final AssigneeGetByIdService assigneeGetByIdService;
+
+  public Mono<Assignee> get(Mono<String> assigneeId) {
+    return assigneeGetByIdService.get(assigneeId);
+  }
+
+}
--- assignee-ms/src/main/java/com/cardonamaturana/assigneems/infrastructure/api/controller/AssigneeController.java
+++ assignee-ms/src/main/java/com/cardonamaturana/assigneems/infrastructure/api/controller/AssigneeController.java
@@ -3,6 +3,7 @@ package com.cardonamaturana.assigneems.infrastructure.api.controller;
 import com.cardonamaturana.assigneems.application.assignee.AssigneeDeleteByIdApplication;
 import com.cardonamaturana.assigneems.application.assignee.AssigneeGetAllApplication;
 import com.cardonamaturana.assigneems.application.assignee.AssigneeGetByEmailApplication;
+import com.cardonamaturana.assigneems.application.assignee.AssigneeGetByIdApplication;
 import com.cardonamaturana.assigneems.infrastructure.api.dto.response.assignee.AssigneeResponse;
 import com.cardonamaturana.assigneems.infrastructure.api.mapper.assignee.AssigneeResponseMapper;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -25,6 +27,7 @@ public class AssigneeController {
 
   private final AssigneeGetAllApplication assigneeGetAllApplication;
   private final AssigneeGetByEmailApplication assigneeGetByEmailApplication;
+  private final AssigneeGetByIdApplication assigneeGetByIdApplication;
   private final AssigneeDeleteByIdApplication assigneeDeleteByIdApplication;
   private final AssigneeResponseMapper assigneeResponseMapper;
 
@@ -38,13 +41,23 @@ public class AssigneeController {
     return assigneeGetAllApplication.getAll().map(assigneeResponseMapper::toDto);
   }
 
-  @GetMapping("/email")
+  @GetMapping("/{email}")
   @Operation(summary = "Get Assignee for personal email", description = "Obtain one register of assignee for personal email", responses = {
       @ApiResponse(responseCode = "200", description = "obtained successfully"),
       @ApiResponse(responseCode = "500", description = "error in response")})
   @ResponseStatus(HttpStatus.OK)
-  public Mono<AssigneeResponse> getAssgigneeByEmail(String assigneeEmail) {
+  public Mono<AssigneeResponse> getAssigneeByEmail(@RequestParam("email") String assigneeEmail) {
     return assigneeGetByEmailApplication.get(Mono.just(assigneeEmail))
+        .map(assigneeResponseMapper::toDto);
+  }
+
+  @GetMapping("/id")
+  @Operation(summary = "Get Assignee for id ", description = "Obtain one register of assignee for id", responses = {
+      @ApiResponse(responseCode = "200", description = "obtained successfully"),
+      @ApiResponse(responseCode = "500", description = "error in response")})
+  @ResponseStatus(HttpStatus.OK)
+  public Mono<AssigneeResponse> getAssigneeById(@RequestParam("assigneeId") String assigneId) {
+    return assigneeGetByIdApplication.get(Mono.just(assigneId))
         .map(assigneeResponseMapper::toDto);
   }
 
